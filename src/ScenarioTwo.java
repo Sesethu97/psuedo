a968d283f0e0770342bf9c512d5f51604bd0e133
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-05-16 16:41:47 +0200

    Average

--- src/ScenarioTwo.java
+++ src/ScenarioTwo.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ScenarioTwo {
     public static void main(String[] args) {
+        int count, i;
+
+        float sum = 0, mean;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Number of Elements");
+        count = input.nextInt();
+
+        System.out.println("Enter " + count + " Elements");
+        for (i = 0; i < count; i++) {
+            sum += input.nextInt();
+        }mean = sum / count;
+
+        System.out.println("Mean : " + mean);
 
     }
 }
